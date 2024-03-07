62a6c054e4776db5494dd8e5e62c3a71ff57e183
Dmitry S <hedead@mail.ru>
2024-03-07 23:50:58 +0700

    Рабочая версия если в теле письма (стр 27-28) латинские буквы. с Русскими выдает ошибку

--- to email.py
+++ to email.py
@@ -15,7 +15,7 @@ with open("data.txt", "r") as file:
 
 # Параметры SMTP сервера
 smtp_server = 'smtp.yandex.com'
-port = 465  #с безопасным соединением порт 465. В случае, если вы не можете воспользоваться безопасным соединением, вы можете подключиться к SMTP-серверу по портам 25 или 587
+port = 587  #с безопасным соединением порт 465. В случае, если вы не можете воспользоваться безопасным соединением, вы можете подключиться к SMTP-серверу по портам 25 или 587
 email_user = 'Garant.ZV@yandex.ru'
 email_password = 'ibmflvtbhwzcuevg' # пароль почты
 
@@ -23,30 +23,32 @@ email_password = 'ibmflvtbhwzcuevg' # пароль почты
 # Создаем сообщение
 msg = MIMEMultipart()
 msg['From'] = email_user
-msg['Subject'] = 'Test email with attachments'
-body = 'This is a test email with attachments.'
+msg['Subject'] = 'Коммерческое предложение. Ремонт подъездов. Отмостки. Кровля'
+body = 'Фирма Гарант производит работы по ремонту подъездов МКД, ремонту и устройству отмостки, обустройство кровли. Реставрируем входные двери. Низкие цены! Смета бесплатно! Фирма Гарант предлагает взаимовыгодное сотрудничество! тел +7-913-940-4833'
+#body = 'Text'
 msg.attach(MIMEText(body, 'plain'))
 
 # Прикрепляем файл к сообщению
-filename = 'Garant_KP.pdf'
-attachment = open(filename, 'rb')
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-msg.attach(part)
+#filename = 'Garant_KP.pdf'
+#attachment = open(filename, 'rb')
+#part = MIMEBase('application', 'octet-stream')
+#part.set_payload((attachment).read())
+#encoders.encode_base64(part)
+#part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+#msg.attach(part)
 
 # Отправляем письмо каждому адресу в списке
 for email in emails:
     msg['To'] = email
+    print(email)
     print(1)
     
-    server = smtplib.SMTP_SSL(smtp_server, port)
-#    server.starttls()
+    server = smtplib.SMTP(smtp_server, port)
+    server.starttls()
     print(2)
     server.login(email_user, email_password)
     print(3)
-    server.send_message(msg)
+    server.send_message(msg, to_addrs=email)
     server.quit()
 
 print("Emails sent successfully.")
