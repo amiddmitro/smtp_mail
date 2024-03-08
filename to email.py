b870f2f5f762a1bac6f9c94c6611145ef0511532
Dmitry S <hedead@mail.ru>
2024-03-08 11:13:14 +0700

    Рабочая версия! Иногда, почтовые ящики принимают письмо как спам. Было бы неплохо сделать счётчик, который показывал бы, какой ящик не существует, какой воспринял как спам...

--- to email.py
+++ to email.py
@@ -15,7 +15,7 @@ with open("data.txt", "r") as file:
 
 # Параметры SMTP сервера
 smtp_server = 'smtp.yandex.com'
-port = 587  #с безопасным соединением порт 465. В случае, если вы не можете воспользоваться безопасным соединением, вы можете подключиться к SMTP-серверу по портам 25 или 587
+port = 465  #с безопасным соединением порт 465. В случае, если вы не можете воспользоваться безопасным соединением, вы можете подключиться к SMTP-серверу по портам 25 или 587
 email_user = 'Garant.ZV@yandex.ru'
 email_password = 'ibmflvtbhwzcuevg' # пароль почты
 
@@ -23,32 +23,34 @@ email_password = 'ibmflvtbhwzcuevg' # пароль почты
 # Создаем сообщение
 msg = MIMEMultipart()
 msg['From'] = email_user
-msg['Subject'] = 'Коммерческое предложение. Ремонт подъездов. Отмостки. Кровля'
-body = 'Фирма Гарант производит работы по ремонту подъездов МКД, ремонту и устройству отмостки, обустройство кровли. Реставрируем входные двери. Низкие цены! Смета бесплатно! Фирма Гарант предлагает взаимовыгодное сотрудничество! тел +7-913-940-4833'
+msg['Subject'] = 'Коммерческое предложение. Ремонт подъездов. Отмостки. Кровля. тел. +7-913-940-4833'
+body = 'ООО Гарант предлагает услуги по комплексному ремонту подъездов. NEW! Реставрация входных дверей.'
 #body = 'Text'
 msg.attach(MIMEText(body, 'plain'))
 
 # Прикрепляем файл к сообщению
-#filename = 'Garant_KP.pdf'
-#attachment = open(filename, 'rb')
-#part = MIMEBase('application', 'octet-stream')
-#part.set_payload((attachment).read())
-#encoders.encode_base64(part)
-#part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-#msg.attach(part)
+filename = 'Garant_KP.pdf'
+attachment = open(filename, 'rb')
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+msg.attach(part)
 
 # Отправляем письмо каждому адресу в списке
+# 
 for email in emails:
     msg['To'] = email
     print(email)
     print(1)
     
-    server = smtplib.SMTP(smtp_server, port)
-    server.starttls()
+    server = smtplib.SMTP_SSL(smtp_server, port)
+# при использовании SMTP_SSL (port 465) следующая строка отключается. Если использовать SMTP (port 587), то строка включается
+#   server.starttls()   
     print(2)
     server.login(email_user, email_password)
     print(3)
-    server.send_message(msg, to_addrs=email)
+    server.send_message(msg= msg, to_addrs=email)
     server.quit()
 
 print("Emails sent successfully.")
